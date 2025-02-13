f8440bcd4436dbf303b868eadce04e2f7d2d751b
Andrew Butler <abutler911@gmail.com>
2025-02-12 22:38:31 -0800

    added folder structure

--- server.js
+++ server.js
@@ -1,14 +1,24 @@
 const express = require("express");
 const path = require("path");
+const expressLayouts = require("express-ejs-layouts");
+
 const app = express();
 const port = process.env.PORT || 3000;
+
+// View engine
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
 
 // Middleware
 app.use(express.static(path.join(__dirname, "public")));
 
+// Express Layouts
+app.use(expressLayouts);
+app.set("layout", "layouts/main");
+
 // Routes
 app.get("/", (req, res) => {
-  res.render("index");
+  res.render("index", { title: "Home" });
 });
 
 // Error handling
